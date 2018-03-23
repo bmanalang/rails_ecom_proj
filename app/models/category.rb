e17bfefeaa8ec884ee3e06ab5720a106d2fdd371
Bernadette Manalang <b_manalang@outlook.com>
2018-03-23 04:57:55 +0000

    Whitelisted cupcakes and categories params.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :cupcakes
+  validates :name, presence: true
 end
--- app/models/cupcake.rb
+++ app/models/cupcake.rb
@@ -1,3 +1,4 @@
 class Cupcake < ApplicationRecord
   belongs_to :category
+  validates :name, presence: true
 end

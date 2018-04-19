02fd5fd69b79c126d18c736a6b643ef844d4a2b2
Bernadette Manalang <b_manalang@outlook.com>
2018-04-19 15:10:27 +0000

    Tried to clean up rubocop offences, but found myself in a lot of files that were auto generated.  Did a bunch of replacing double quotes with single quotes.

--- db/schema.rb
+++ db/schema.rb
@@ -12,84 +12,92 @@
 
 ActiveRecord::Schema.define(version: 20180329064929) do
 
-  create_table "abouts", force: :cascade do |t|
-    t.string "description"
-    t.string "address"
-    t.string "telephone"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'abouts', force: :cascade do |t|
+    t.string 'description'
+    t.string 'address'
+    t.string 'telephone'
+    t.string 'email'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string "namespace"
-    t.text "body"
-    t.string "resource_type"
-    t.integer "resource_id"
-    t.string "author_type"
-    t.integer "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
-    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
-    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  create_table 'active_admin_comments', force: :cascade do |t|
+    t.string 'namespace'
+    t.text 'body'
+    t.string 'resource_type'
+    t.integer 'resource_id'
+    t.string 'author_type'
+    t.integer 'author_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['author_type', 'author_id'],
+            name: 'index_active_admin_comments_on_author_type_and_author_id'
+    t.index ['namespace'],
+            name: 'index_active_admin_comments_on_namespace'
+    t.index ['resource_type',
+             'resource_id'],
+            name: 'index_active_admin_comments_on_resource_type_and_resource_id'
   end
 
-  create_table "admin_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  create_table 'admin_users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string 'current_sign_in_ip'
+    t.string 'last_sign_in_ip'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'],
+            name: 'index_admin_users_on_email', unique: true
+    t.index ['reset_password_token'],
+            name: 'index_admin_users_on_reset_password_token', unique: true
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "cupcakes", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.float "price"
-    t.string "image"
-    t.integer "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_cupcakes_on_category_id"
+  create_table 'cupcakes', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.float 'price'
+    t.string 'image'
+    t.integer 'category_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'],
+            name: 'index_cupcakes_on_category_id'
   end
 
-  create_table "order_items", force: :cascade do |t|
-    t.integer "quantity", default: 0, null: false
-    t.decimal "price", precision: 15, scale: 2, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'order_items', force: :cascade do |t|
+    t.integer 'quantity', default: 0, null: false
+    t.decimal 'price', precision: 15, scale: 2, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "last_name", null: false
-    t.decimal "sub_total"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "order_id"
-    t.string "token"
-    t.index ["order_id"], name: "index_orders_on_order_id"
+  create_table 'orders', force: :cascade do |t|
+    t.string 'first_name', null: false
+    t.string 'last_name', null: false
+    t.decimal 'sub_total'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'order_id'
+    t.string 'token'
+    t.index ['order_id'],
+            name: 'index_orders_on_order_id'
   end
 
-  create_table "shopping_carts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'shopping_carts', force: :cascade do |t|
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
 end
--- test/controllers/cupcakes_controller_test.rb
+++ test/controllers/cupcakes_controller_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class CupcakesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get cupcakes_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get cupcakes_show_url
     assert_response :success
   end
